8dd005375844994798609b0a3f13ad362d08182a
anshuman-0906 <anshuman.mishra0906@gmail.com>
2025-11-02 08:04:02 +0530

    cd3

---
+++ cd3.java
@@ -0,0 +1,9 @@
+public class cd3 {
+    public static void main(String[] args) {
+        char ch=args[0].charAt(0);
+
+        System.out.println("ASCII value of "+ch+" is "+(int)ch);
+    
+    }
+    
+}
