618aebea9d2fde653ee4ef4613b69d56b8fd13ea
anshuman-0906 <anshuman.mishra0906@gmail.com>
2025-11-02 15:49:33 +0530

    cd4

---
+++ cd4.java
@@ -0,0 +1,7 @@
+public class cd4 {
+    public static void main(String[] args) {
+        double a = Double.parseDouble(args[0]);
+        System.out.println(Math.cos(5*a)+Math.sin(7*a));
+    }
+    
+}
